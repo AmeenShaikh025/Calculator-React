1d1951aa286576dc30381780f31de6da87892d48
ameen025 <ameenshaikh25031994@gmail.com>
2020-02-01 22:09:36 +0530

    build

---
+++ build/precache-manifest.20356aa470cf5c31440ac3a1cdf7a548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af8b435e8b1f7110563be00a9e0adc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c8111af832b8fa79a7b",
+    "url": "/static/css/main.70eb884c.chunk.css"
+  },
+  {
+    "revision": "968f027c1763b0f7db0b",
+    "url": "/static/js/2.ea9a950d.chunk.js"
+  },
+  {
+    "revision": "a7eed2f310b82a6d5c4409c9acf7cf5a",
+    "url": "/static/js/2.ea9a950d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8111af832b8fa79a7b",
+    "url": "/static/js/main.030baa4c.chunk.js"
+  },
+  {
+    "revision": "0d76f63897281dc8f6ad",
+    "url": "/static/js/runtime-main.5f48bb11.js"
+  }
+]);
